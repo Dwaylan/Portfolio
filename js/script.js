ab6b088b62ed44fc210db7422b3d6f8b3cb0fa77
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-06-15 18:26:35 -0400

    fixed image overlay

--- js/script.js
+++ js/script.js
@@ -54,3 +54,14 @@ $(window).scroll(function () {
     });
   }
 });
+
+$("[data-fancybox]").fancybox();
+
+$(".items").isotope({
+  filter: "*",
+  animationOptions: {
+    duration: 1500,
+    easing: "linear",
+    queue: false,
+  },
+});
