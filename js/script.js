3d46c743c33263820e798d876dd4c01d6f1de9e5
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-06-16 19:28:13 -0400

    sticky nav in process

--- js/script.js
+++ js/script.js
@@ -83,3 +83,28 @@ $("#filters a").click(function () {
 
   return false;
 });
+
+const nav = $("#navigation");
+const navTop = nav.offset().top;
+
+// $(window).on("scroll", stickyNavigation);
+
+// function stickyNavigation() {
+//   const body = $("body");
+
+//   if ($(window).scrollTop() >= navTop) {
+//     body.css("padding-top", nav.outerHeight() + "px");
+//     body.addClass("fixedNav");
+//   } else {
+//     body.css("padding-top", 0);
+//     body.removeClass("fixedNav");
+//   }
+// }
+
+// $("#navigation li a").click(function (e) {
+//   e.preventDefault();
+
+//   var targetElement = $(this).attr("href");
+//   var targetPosition = $(targetElement).offset.top;
+//   $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+// });
